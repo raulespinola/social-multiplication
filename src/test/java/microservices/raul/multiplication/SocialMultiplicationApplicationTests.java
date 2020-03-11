2058c8ebfaa148a058f333f683d4c809800e7d7c
Raul <raulespinola@gmail.com>
2020-03-11 17:29:16 -0300

    Proyect Microservices

---
+++ src/test/java/microservices/raul/multiplication/SocialMultiplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package microservices.raul.multiplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMultiplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
